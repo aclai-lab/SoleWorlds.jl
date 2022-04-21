857d4cde85a28fe9b4a636b4170dd8e11ae18e2a
paraandrea <andrea.paradiso.uni@gmail.com>
2022-04-21 20:45:28 +0200

    traits worlds

--- src/SoleWorlds.jl
+++ src/SoleWorlds.jl
@@ -1,42 +1,37 @@
 module SoleWorlds
 
-    abstract type AbstractWorld end
+#abstract types
+abstract type AbstractWorld end
+abstract type ShapeWorld    <: AbstractWorld end
 
-    struct UndefWorld       <: AbstractWorld
-        value::Union{Number,Array{<:Number}}
+#concrete types
+struct PointWorld           <: AbstractWorld
+    coord::Real
+end
+
+struct HyperRectangle{N}    <: ShapeWorld
+    coords::Vector{<:NTuple{2,<:Real}}
+
+    function HyperRectangle{N}(coords::Vector{<:NTuple{2,<:Real}}) where N
+        @assert N == length(coords)
+        return new{N}(coords)
     end
 
-    struct Point            <: AbstractWorld
-        value::Number
+    function HyperRectangle(coords::Vector{<:NTuple{2,<:Real}})
+        return HyperRectangle{length(coords)}(coords)
     end
+end
 
-    struct Intervall        <: AbstractWorld
-        values::Array{<:Number,1}
-    end
+#alias
+const Intervall         = HyperRectangle{1}
+const Rectangle         = HyperRectangle{2}
+const Parallelepiped    = HyperRectangle{3}
 
-    struct Rectangle        <: AbstractWorld
-        values::Array{<:Number,2}
-    end
-
-    struct Parallelepiped   <: AbstractWorld
-        values::Array{<:Number,3}
-    end
-
-    struct HyperRectangle   <: AbstractWorld
-        values::Array{<:Number}
-
-        function HyperRectangle(values::Array{<:Number})
-            @assert ndims(values) >= 4 "HyperRectangle is not defined for less than 4 dimensions\n"
-        end
-    end
-
-
-    show(io::IO,x::UndefWorld)      = println(io,typeof(x)," with value ", x.value);
-    show(io::IO,x::Point)           = println(io,typeof(x)," with value ", x.value);
-    show(io::IO,x::Intervall)       = println(io,typeof(x)," with values ", x.values);
-    show(io::IO,x::Rectangle)       = println(io,typeof(x)," with values ", x.values);
-    show(io::IO,x::Parallelepiped)  = println(io,typeof(x)," with values ", x.values);
-    show(io::IO,x::HyperRectangle)  = println(io,typeof(x), " with values ",x.values,
-                                            " on ", ndims(x), " dimensions");
+#shows
+show(world::PointWorld)      = println(typeof(World)," with values ",world.coord)
+show(world::HyperRectangle)  = println(typeof(world)," with values ",world.coords)
+show(world::Intervall)       = println(typeof(world)," with values ",world.coords)
+show(world::Rectangle)       = println(typeof(world)," with values ",world.coords)
+show(world::Intervall)       = println(typeof(world)," with values ",world.coords)
 
 end
