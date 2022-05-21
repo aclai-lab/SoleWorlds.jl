3bc4261efd6cceacf2be1a8641cf3ac931135dba
paraandrea <andrea.paradiso.uni@gmail.com>
2022-05-21 15:34:57 +0200

    added worldGen

--- src/SoleWorlds.jl
+++ src/SoleWorlds.jl
@@ -2,6 +2,7 @@ module SoleWorlds
 
 # imports/usings
 import Base: show
+import IterTools
 
 # exports
 export IntervalWorld, RectangleWorld, CubeWorld
@@ -36,5 +37,40 @@ const CubeWorld      = HyperRectangleWorld{3}
 #shows
 show(io::IO, w::PointWorld)                         = print(io, typeof(w), ": ", w.coord)
 show(io::IO, w::T) where T<:HyperRectangleWorld     = print(io, typeof(w), ": ", w.coords)
+
+
+# worlds generators
+
+function PointWorldGen(d::Int64)
+
+    return worlds = [PointWorld(i) for i in 1:d]
+
+end
+
+function worldGen(d::Int64)
+    return worlds = [HyperRectangleWorld{1}([x]) for x in IterTools.subsets([1:d...],Val(2))]
+end
+
+function worldGen(d::NTuple{2,Int64})
+
+    return worlds = [
+        HyperRectangleWorld{2}([x,y])
+        for y in IterTools.subsets([1:d[2]...],Val(2))
+        for x in IterTools.subsets([1:d[1]...],Val(2))
+    ]
+
+end
+
+function worldGen(d::NTuple{3,Int64})
+
+    return worlds = [
+        HyperRectangleWorld{3}([x,y,z])
+        for z in IterTools.subsets([1:d[3]...],Val(2))
+        for y in IterTools.subsets([1:d[2]...],Val(2))
+        for x in IterTools.subsets([1:d[1]...],Val(2))
+    ]
+
+end
+
 
 end
